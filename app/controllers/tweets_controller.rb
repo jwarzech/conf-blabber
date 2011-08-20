2fa5177dd8fa100a7d5c7e433e64acacfd9e74c6
Jordan Warzecha <jwarzech@gmail.com>
2011-08-20 00:58:48 -0400

    Added additional checking of after parameter.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    unless params[:after] == 0
+    unless params[:after].nil? || params[:after] == 0
       @tweets =  Tweet.next_set(params[:after])
     else
       @tweets = Tweet.latest_set
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
   SET_SIZE = 5
-  scope :latest_set, limit(SET_SIZE).order("id desc")
+  scope :latest_set, order("id desc").limit(SET_SIZE)
   scope :next_set, lambda { |latest_id| where("id > ?", latest_id).limit(SET_SIZE) }
 end
